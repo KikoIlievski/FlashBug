d30d17bfd03480de1d4ecc1de1594feded8353bb
Kristijan Ilievski <kikojan16@gmail.com>
2020-09-28 11:44:24 +1300

    Set slider values and event trigger on change

--- popup/popup.js
+++ popup/popup.js
@@ -3,6 +3,15 @@ let myButton = document.getElementById("sayHello");
 myButton.onclick = function(element){
     chrome.runtime.sendMessage({"txt":"message has gone through!"})
 }
+$("#slider").slider( { 
+    min:0,
+    max:255
+})
 $(function(){
-    $("#slider").slider();
+    $("#slider").slider({
+        change: function(event, ui) { 
+            let value = $("#slider").slider("values", 0 );
+            console.log(value)
+        }
+    });
 });
