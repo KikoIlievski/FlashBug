b910ab87c97476b44e8353bda1a5e921f656da13
Kristijan Ilievski <kikojan16@gmail.com>
2020-10-07 17:37:49 +1300

    numBlobs are able to be changed live, also cleaned up bug where reference to BGscript was not removed from earlier commit.

--- popup/popup.js
+++ popup/popup.js
@@ -1,31 +1,34 @@
+// referencing slider objects
 var redSlider = $(".red");
 var greenSlider = $(".green");
 var blueSlider = $(".blue");
+var numBlobsInput = document.getElementById("numBlobsInput");
 
+// setting values of sliders to value in storage (ie remembering old settings)
 redSlider.slider({
     create: chrome.storage.sync.get("red", result=>{
             redSlider.slider("value", result["red"]);
     })
 });
-
 greenSlider.slider({
     create: chrome.storage.sync.get("green", result=>{
             greenSlider.slider("value", result["green"]);
     })
 });
-
 blueSlider.slider({
     create: chrome.storage.sync.get("blue", result=>{
             blueSlider.slider("value", result["blue"]);
     })
 });
 
+// setting the properties for all the sliders (max values, min values, step)
 $(".colourSlider").slider( { 
     min:0,
     max:1,
     step:0.01
 })
 
+// handling changes in sliders by user
 redSlider.slider({
     change: function(event) { 
         if (event.originalEvent){
@@ -49,4 +52,17 @@ blueSlider.slider({
             chrome.storage.sync.set({"blue": value});
         }
     }
+});
+
+// initialising the starting value for the numBlobs
+chrome.storage.sync.get("numBlobs", result=>{ 
+    if (!result["numBlobs"]){ 
+        chrome.storage.sync.set({"numBlobs":5});
+    } else { 
+        numBlobsInput.value = result["numBlobs"];
+    }
+});
+// writing to chrome storage when input box is changed
+numBlobsInput.addEventListener("input", event=>{ 
+    chrome.storage.sync.set({"numBlobs":numBlobsInput.value});
 });
